67c5e4d59d0ecb7e68a7d8a1a1885e873f6dd485
tracer-nju <tracer@nju.edu.cn>
2019-03-26 21:47:52 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: ff703afebb7f42599dc571e7601337ad   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  21:47:52 up  2:51,  5 users,  load average: 0.11, 0.17, 0.22

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <dirent.h>
+#include <string.h>
 
 int a_op[3];//分别代表-p，-n和-V是否存在
 
@@ -9,11 +10,11 @@ int main(int argc, char *argv[]) {
   int i;		
   for (i = 0; i < argc; i++) { 
     assert(argv[i]); // always true
-	if(argv[i]=="-p"||argv[i]=="--show-pids")
+	if(!strcmp(argv[i],"-p")||!strcmp(argv[i],"--show-pids"))
 		a_op[0] = 1;
-	if(argv[i]=="-n"||argv[i]=="--numeric-sort")
+	if(!strcmp(argv[i],"-n")||!strcmp(argv[i],"--numeric-sort"))
 		a_op[1] = 1;
-	if(argv[i]=="-V"||argv[i]=="--version")
+	if(!strcmp(argv[i],"-V")||!strcmp(argv[i],"--version"))
 		a_op[2] = 1;
     //printf("argv[%d] = %s\n", i, argv[i]); //teacher's code
   }
