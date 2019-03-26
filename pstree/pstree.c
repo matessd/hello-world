dcf97e94fac1ba85eadf4838051f8f7bdfa4820f
tracer-nju <tracer@nju.edu.cn>
2019-03-26 21:43:03 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: ff703afebb7f42599dc571e7601337ad   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  21:43:03 up  2:46,  5 users,  load average: 0.18, 0.16, 0.24

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <assert.h>
+#include <dirent.h>
+
+int a_op[3];//分别代表-p，-n和-V是否存在
 
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
-  int i;
-  for (i = 0; i < argc; i++) {
+  //printf("Hello, World!\n");
+  int i;		
+  for (i = 0; i < argc; i++) { 
     assert(argv[i]); // always true
-    printf("argv[%d] = %s\n", i, argv[i]);
+	if(argv[i]=="-p"||argv[i]=="--show-pids")
+		a_op[0] = 1;
+	if(argv[i]=="-n"||argv[i]=="--numeric-sort")
+		a_op[1] = 1;
+	if(argv[i]=="-V"||argv[i]=="--version")
+		a_op[2] = 1;
+    //printf("argv[%d] = %s\n", i, argv[i]); //teacher's code
   }
   assert(!argv[argc]); // always true
+  
+  //below is read dir
+  DIR *dir = opendir("/proc");
+  assert(dir);
+  struct dirent *ptr;
+  while(ptr = readdir(dir)){
+     make{
+         i{
+    
+         }
+     } 
+  }
   return 0;
 }
