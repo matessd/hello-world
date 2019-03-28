dd011588632166b3c1eedac2e1cadcaec570075f
tracer-nju <tracer@nju.edu.cn>
2019-03-28 16:19:16 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: cabfcbcd8efd4683931e7265d7a4c869   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  16:19:16 up  2:12,  5 users,  load average: 0.26, 0.25, 0.19

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -184,7 +184,7 @@ void fnMake_tree(){
     memset(aa_out,' ',sizeof(aa_out));
     int line_cnt = fnDFS(1,a_process[1].name, 0, 0);
     for(int i=0; i<line_cnt; i++){
-        //puts(&aa_out[i][0]);
+        puts(&aa_out[i][0]);
     }   
 }
 
