23f7904b786835cbf54e976e24e4bf434815fe98
tracer-nju <tracer@nju.edu.cn>
2019-03-28 15:02:34 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: cabfcbcd8efd4683931e7265d7a4c869   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:02:34 up 55 min,  5 users,  load average: 0.12, 0.31, 0.28

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -44,11 +44,11 @@ void fnRead_data(){
           struct dirent *sub_ptr;
           sprintf(dir_name,"/proc/%s/task",ptr->d_name);
           DIR* sub_dir = opendir(dir_name);
-          sub_ptr = readdir(sub_dir);
+          //sub_ptr = readdir(sub_dir);
           //跳过task中的第一个文件夹
           while( (sub_ptr = readdir(sub_dir)) ){
               sprintf(sub_file_name,"%s/%s/status",dir_name,sub_ptr->d_name);
-              printf("%s\n",sub_file_name);
+              //printf("%s\n",sub_file_name);
               fp = fopen(sub_file_name,"r");
               fnRead_proc(fp);
           } 
