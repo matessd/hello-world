b2c15cb0b72c0067e1d87f99875dfdbfb5fd791d
tracer-nju <tracer@nju.edu.cn>
2019-03-28 17:02:53 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: cabfcbcd8efd4683931e7265d7a4c869   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  17:02:53 up  2:56,  4 users,  load average: 0.04, 0.10, 0.15

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -162,8 +162,8 @@ int fnDFS(int pid,  char* name, int x, int y){
                     aa_out[x+j][y] = '|';
                 }
                 x+=width;
-                strcpy(&aa_out[x][y+1],"──");
-                width = fnDFS(child_pid, a_process[i].name,x, y+7);
+                strcpy(&aa_out[x][y+1],"──-");
+                width = fnDFS(child_pid, a_process[i].name,x, y+8);
                 break;
             }
             if(i==a_pid_num)
