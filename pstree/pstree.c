2655b0643e4f165aaaabb177433832f149e3483c
tracer-nju <tracer@nju.edu.cn>
2019-03-29 11:25:45 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: cabfcbcd8efd4683931e7265d7a4c869   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:25:45 up 21:18,  7 users,  load average: 0.11, 0.05, 0.08

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -185,7 +185,7 @@ int fnDFS(int pid,  char* name, int x, int y){
                     x+=width;
                     aa_out[x][y] = (char)0x3;
                 }
-                width = fnDFS(child_pid, a_process[i].name, x, y);
+                width = fnDFS(child_pid, a_process[i].name, x, y+1);
                 break;
             }
             if(i==a_pid_num)
@@ -209,8 +209,8 @@ void fnMake_tree(){
     int line_cnt = fnDFS(1,a_process[1].name, 0, 0);
     strcpy(&aa_chSpec[1][0],"─┬─");
     strcpy(&aa_chSpec[2][0],"│");
-    strcpy(&aa_chSpec[3][0],"├─ ");
-    strcpy(&aa_chSpec[4][0],"└─ ");
+    strcpy(&aa_chSpec[3][0],"├─");
+    strcpy(&aa_chSpec[4][0],"└─");
     for(int i=0; i<line_cnt; i++){
         int j = -1;
         while(aa_out[i][++j]!='\0'){
