c45f52b5ea845c06dd8dd678b8cd132e3c450452
tracer-nju <tracer@nju.edu.cn>
2019-05-05 12:52:52 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: deff4253d347480582b815ecb937854f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  12:52:52 up  2:08,  3 users,  load average: 1.16, 0.33, 0.20

--- libco/co.c
+++ libco/co.c
@@ -1,20 +1,88 @@
 #include <stdio.h>
 #include "co.h"
+#include <setjmp.h>
+
+#if defined(__i386__)
+    #define SP "%%esp"
+#elif defined(__x86_64__)
+    #define SP "%%rsp"
+#endif
 
 struct co {
+    int if_run;
+    char stack[4096];
+    jmp_buf buf[1024];
 };
 
+struct co *current = NULL;
+struct co *coroutines[5];
+int g_cnt = 0;
+void *__stack_backup[5];
+
 void co_init() {
+    for(int i=0; i<5; i++)
+        coroutines[i] = NULL;
+    current = coroutines[0] = (co*)malloc(sizeof(co));//泄漏？
+    assert(!current);
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
-  func(arg); // Test #2 hangs
-  return NULL;
+  co* new = (co *)malloc(sizeof(co));
+  assert(!new);
+  current = new;
+  for(int i=1; i<5; i++){
+      if(!coroutines[i]){
+          coroutines[i] = current;
+          break;
+      }
+  }
+  if(setjmp(coroutines[0]->buf)==0){
+      asm volatile("mov " SP ", %0; mov %1, " SP :
+                   "=g"(__stack_backup[0]) :
+                   "g"(current->stack + sizeof(current->stack)));
+      func(arg);
+      asm volatile("mov %0," SP : : "g"(__stack_backup[0]));
+  }
+  //func(arg); // Test #2 hangs
+  else{
+      asm volatile("mov %0," SP : : "g"(__stack_backup[0]));
+      current = coroutines[0];
+  } 
+  return new;
 }
 
 void co_yield() {
+  if(setjmp(current->buf)==0){
+       current->if_run = 1;
+       for(int i=0; i<5; i++){
+           g_cnt = (g_cnt+1)%5;
+           if(!coroutines[g_cnt])
+               break;
+       }
+       current = coroutines[g_cnt];
+       longjmp(current->buf,1);
+  }else{
+       current->if_run = 0;
+       return;
+  }
 }
 
 void co_wait(struct co *thd) {
+  current = coroutines[0];
+  while(thd->if_run){
+      if(setjmp(current->buf)==0){
+          current = thd;
+          longjmp(current->buf,1);
+      }else{
+          current = coroutines[0];
+      }
+  }
+  free(thd);
+  for(int i=0; i<5; i++){
+      if(coroutines[i]==thd){
+          coroutines[i]=NULL;
+          break;
+      }
+  }
 }
 
