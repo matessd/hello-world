959b9038a97ec3ffa787f739e6b6bed3959e5e99
tracer-nju <tracer@nju.edu.cn>
2019-05-09 15:09:09 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 9f3d818c898a4ec39059aec4e7394bca   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:09:09 up  7:04,  7 users,  load average: 0.09, 0.14, 0.15

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -74,7 +74,7 @@ void init_game(){
 }
 
 void game_draw(int block_num, uint32_t color){
-    draw_rect(block_pos[block_num][0], block_pos[block_num][1], w/4, h/4, color);
+    draw_rect(block_pos[block_num][0], block_pos[block_num][1], SIDE, SIDE, color);
 }
 
 void draw_rect(int x, int y, int w, int h, uint32_t color) {
