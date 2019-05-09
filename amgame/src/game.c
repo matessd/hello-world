46b0f06103c552da2718573f1b09f69d3901c4db
tracer-nju <tracer@nju.edu.cn>
2019-05-09 14:07:43 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 9f3d818c898a4ec39059aec4e7394bca   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  14:07:43 up  6:03,  7 users,  load average: 0.08, 0.12, 0.15

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,21 +1,36 @@
 #include <game.h>
-
+#include <klib.h>
 void init_screen();
-void splash();
-void read_key();
+//void splash();
+//void read_key();
+
+#define FPS 30
+#define HZ 100
+int w, h;
+int road_w;//my
 
 int main() {
   // Operating system is a C program
   _ioe_init();
   init_screen();
-  splash();
+  /* teacher's code:
+   * splash();
   while (1) {
     read_key();
+  }*/
+  //below is my
+  printf("%d>>%d\n",w/SIDE,w%SIDE);
+  /*unsigned long time, frames=0, next_frame=0, next_refresh=0;
+  bool redraw = false;
+  while(1){
+      time = uptime();
+      if(time >= next_frame) break;
   }
+  frame++;*/
   return 0;
 }
 
-void read_key() {
+/*void read_key() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
@@ -28,18 +43,17 @@ void read_key() {
     puts(key_names[event.keycode]);
     puts("\n");
   }
-}
-
-int w, h;
+}*/
 
 void init_screen() {
   _DEV_VIDEO_INFO_t info = {0};
   _io_read(_DEV_VIDEO, _DEVREG_VIDEO_INFO, &info, sizeof(info));
   w = info.width;
   h = info.height;
+
 }
 
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
+/*void draw_rect(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: allocated on stack
   _DEV_VIDEO_FBCTL_t event = {
     .x = x, .y = y, .w = w, .h = h, .sync = 1,
@@ -49,9 +63,9 @@ void draw_rect(int x, int y, int w, int h, uint32_t color) {
     pixels[i] = color;
   }
   _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &event, sizeof(event));
-}
+}*/
 
-void splash() {
+/*void splash() {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
@@ -59,4 +73,4 @@ void splash() {
       }
     }
   }
-}
+}*/
