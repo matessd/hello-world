5dd86ce7810427098f24ce563dc2677d36c45055
tracer-nju <tracer@nju.edu.cn>
2019-05-10 11:06:00 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 9d0e94f163834cbbb3a32cea818d5a45   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:06:00 up  2:02,  8 users,  load average: 0.27, 0.20, 0.11

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,17 +1,31 @@
 #include <game.h>
 #include <klib.h>
+#define FPS 30
+#define genHZ 2//每秒生成两次地鼠
+#define ctlHZ 100//每秒处理100次输入
+//函数
 void init_screen();
 void new_draw_rect(int x, int y, int w, int h, uint32_t color);
 void init_game();
-void game_draw(int block_num, uint32_t color);
+void game_draw();
+void gen_mouse();
+void game_ctl();
 //void splash(int x,y);
-//void read_key();
+int read_key();
 
-#define FPS 30
-#define HZ 100
-int mouse_cnt=0;
+//全局变量
+static int scan_code[]={
+    _KEY_1, _KEY_2, _KEY_3, _KEY_4, _KEY_5,
+    _KEY_6, _KEY_7, _KEY_8, _KEY_9,
+};
+int mouse_cnt=0;//场内地鼠数量
+int kill_cnt=0;//已击中地鼠数量
 //game_init
 int block_pos[9][2];
+int block_state[9];//每格状态
+//game_ctl
+int game_over=0;//是否退出游戏循环
+int game_win=0;//游戏输赢
 
 int main() {
   // Operating system is a C program
@@ -24,33 +38,44 @@ int main() {
   }*/
   //below is my
   init_game();
-  for(int i=0; i<9; i++)
-      game_draw(i, 0xffffff);
-  while(1);
-  /*unsigned long time, frames=0, next_frame=0, next_refresh=0;
-  bool redraw = false;
+  game_draw();
+  unsigned long time, next_gen=0, next_refresh=0, next_ctl=0;;//next_gen是下一次生成地鼠的时间，next_refresh是下一次刷新屏幕的时间,next_ctl是下一次接受玩家输入并处理的时间
+  srand(uptime());
   while(1){
-      time = uptime();
-      if(time >= next_frame) break;
+    while(1){
+        time = uptime();
+        if(time >= next_ctl) break;
+    }
+    next_ctl += 1000/ctlHZ;
+    game_ctl();
+    if(time >= next_gen){
+        gen_mouse();
+        next_gen += 1000/genHZ;
+    }
+    if(game_over==1) break;
+    if(time >= next_refresh){
+        next_refresh += 1000/FPS;
+        game_draw();
+    }
   }
-  frame++;*/
   return 0;
 }
 
-/*void read_key() {
+int read_key() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
-  #define KEYNAME(key) \
+  /*#define KEYNAME(key) \
     [_KEY_##key] = #key,
   static const char *key_names[] = {
     _KEYS(KEYNAME)
-  };
+  };*/
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
-  if (event.keycode != _KEY_NONE && event.keydown) {
+  /*if (event.keycode != _KEY_NONE && event.keydown) {
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
-  }
-}*/
+  }*/
+  return event.keycode;
+}
 
 int w,h;
 void init_screen() {
@@ -71,10 +96,55 @@ void init_game(){
             cnt++;
         }
     }
+    for(int i=0; i<9; i++)
+        block_state[i] = 0;
 }
 
-void game_draw(int block_num, uint32_t color){
-    new_draw_rect(block_pos[block_num][0], block_pos[block_num][1], 88, 80, color);
+void game_draw(){
+    for(int i=0; i<9; i++){
+        if(block_state[i]==0) new_draw_rect(block_pos[i][0], block_pos[i][1], 88, 80, 0xffffff);
+        else  new_draw_rect(block_pos[i][0], block_pos[i][1], 88, 80, 0xff0000);
+    }
+}
+
+void gen_mouse(){
+    int i = rand()%9;
+    while(1){
+        if(block_state[i]==0){
+            block_state[i]=1;
+            mouse_cnt++;
+            break;
+        }
+        i = rand()%9;
+    }
+    if(mouse_cnt==9){
+        game_over=1;
+        printf("Lose! You farm is filled with mice\n");
+    }
+}
+
+void game_ctl(){
+    int wrong_beat = 0;
+    int keycode = read_key();
+    for(int i=0; i<9; i++){
+        if(scan_code[i]==keycode){
+            if(block_state[i]==1){
+                kill_cnt++;
+                block_state[i]=0;
+            }else{
+                wrong_beat = 1;
+            }
+            break;
+        }
+    }
+    if(wrong_beat==1){
+        game_over = 1;
+        printf("Lose! You beat the wrong place\n");
+    }
+    if(kill_cnt==20){
+        game_over = 1;
+        printf("Win! You have killed all 20 mice\n");
+    }
 }
 
 void new_draw_rect(int x, int y, int w, int h, uint32_t color) {
