c809fb65ca59db6135418128964306275faddf8f
tracer-nju <tracer@nju.edu.cn>
2019-05-14 14:09:37 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 7d5b919a42794d4a955be9fb50fbe1ed   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  14:09:37 up  4:03,  7 users,  load average: 0.35, 0.39, 0.36

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -43,8 +43,9 @@ int main(int argc, char *argv[]) {
 
         int null_fd = open("/dev/null",O_WRONLY);
         assert(null_fd>=0);
-        printf("%d\n",null_fd);
+        //printf("%d\n",null_fd);
         assert(dup2(null_fd,1)==1);//stdout
+        close(1);
         //dup2(1, filedes[1]);
         execve("/usr/bin/strace",argv_send,envp);
         printf("execve error, should not reach here\n");
