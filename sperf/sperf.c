97e312a9a3299c82e11a9d39d9d0b44ac4fa8b91
tracer-nju <tracer@nju.edu.cn>
2019-05-14 14:06:47 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 7d5b919a42794d4a955be9fb50fbe1ed   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  14:06:47 up  4:00,  7 users,  load average: 0.28, 0.42, 0.37

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -23,8 +23,8 @@ int main(int argc, char *argv[]) {
     assert(pid>=0);
     if (pid > 0){
         //father
-        close(filedes[1]);
-        close(filedes[0]);
+        //close(filedes[1]);
+        //close(filedes[0]);
         read(filedes[0], buf, sizeof(buf));
         printf("%s>>>>>>>>\n",buf);
     }
