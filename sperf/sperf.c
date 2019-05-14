e10c453992e417467dc65899992d771750641119
tracer-nju <tracer@nju.edu.cn>
2019-05-14 11:18:32 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 7d5b919a42794d4a955be9fb50fbe1ed   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:18:32 up  1:12,  6 users,  load average: 0.28, 0.43, 0.43

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,3 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
 int main(int argc, char *argv[]) {
-  return 0;
+    /*int filedes[2];
+    char buf[80];
+    pid_t pid;
+    
+    pipe( filedes );
+    pid=fork();        
+    if (pid > 0){}
+    else if(pid == 0){}*/
+    printf("%s\n",argv);
+    return 0;
 }
