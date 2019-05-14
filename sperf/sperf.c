866f51564eaad91aa78a5424379b232afea2da8c
tracer-nju <tracer@nju.edu.cn>
2019-05-14 11:44:00 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 7d5b919a42794d4a955be9fb50fbe1ed   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:44:00 up  1:37,  6 users,  load average: 0.18, 0.28, 0.39

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -13,8 +13,14 @@ int main(int argc, char *argv[]) {
     }
     else if(pid == 0){
         assert(argc>1);
-        char *argv_send[] = argv[1];
-        execve("/bin/strace",["strace","-T", argv_send],NULL);
+        char *argv_send[128];
+        argv_send[0] = "strace";
+        argv_send[1] = "-T";
+        for(int i=1; i<argc; i++){
+            argv_send[i+1] = argv[i];
+        }
+        argv_send[argc+1] = NULL;
+        execve("/bin/strace",argv_send,NULL);
     }
     //printf("%s\n",argv[1]);
     return 0;
