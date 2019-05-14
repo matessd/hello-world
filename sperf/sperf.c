196536f947d2056e472b6a7679f09b8f808a3e28
tracer-nju <tracer@nju.edu.cn>
2019-05-14 11:40:31 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 7d5b919a42794d4a955be9fb50fbe1ed   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:40:31 up  1:34,  6 users,  load average: 0.25, 0.39, 0.44

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,14 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <assert.h>
 int main(int argc, char *argv[]) {
-    /*int filedes[2];
+    int filedes[2];
     char buf[80];
     pid_t pid;
     
     pipe( filedes );
     pid=fork();        
-    if (pid > 0){}
-    else if(pid == 0){}*/
-    printf("%s\n",argv[1]);
+    if (pid > 0){
+        
+    }
+    else if(pid == 0){
+        assert(argc>1);
+        char *argv_send[] = argv[1];
+        execve("/bin/strace","strace","-T", argv_send, argv_send,NULL);
+    }
+    //printf("%s\n",argv[1]);
     return 0;
 }
