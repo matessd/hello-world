16eb8ed45545d0ee2105f44e6242bec02c5833b9
tracer-nju <tracer@nju.edu.cn>
2019-05-15 15:57:09 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 173d70bccba94963b34eed4c6114c805   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:57:10 up  6:13, 11 users,  load average: 0.46, 0.46, 0.44

--- kernel/include/my_test.h
+++ kernel/include/my_test.h
@@ -1,5 +1,8 @@
 #include<klib.h>
 #include<common.h>
+#include<my_os.h>
+LOCKDEF(test)
+
 void pmm_test(){
   srand(0);
   char* ptr[4096];
@@ -10,8 +13,10 @@ void pmm_test(){
       printf("fail on %d\n",i);
       return;
     }
+    test_lock();
     sprintf(ptr[i], "hello%d\n",i);
     printf("%s\n",ptr[i]);
+    test_unlock();
   }
   for(int i=0; i<4096; i++)
     printf("%s\n",ptr[i]);
