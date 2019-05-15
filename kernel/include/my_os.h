b099be6aaa0b1be04aa94cd6ba40d55fba8a2797
tracer-nju <tracer@nju.edu.cn>
2019-05-15 11:29:21 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 173d70bccba94963b34eed4c6114c805   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:29:21 up  1:46,  9 users,  load average: 0.27, 0.36, 0.42

---
+++ kernel/include/my_os.h
@@ -0,0 +1,17 @@
+#include<x86.h>
+#include<am.h>
+#define LOCKDEF(name) \
+  static volatile intptr_t name##_locked = 0; \
+  static int name##_lock_flags[8]; \
+  void name##_lock() { \
+    name##_lock_flags[_cpu()] = get_efl() & FL_IF; \
+    cli(); \
+    while (1) { \
+      if (0 == _atomic_xchg(&name##_locked, 1)) break; \
+      pause(); \
+    } \
+  } \
+  void name##_unlock() { \
+    _atomic_xchg(&name##_locked, 0); \
+    if (name##_lock_flags[_cpu()]) sti(); \
+  }
--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -7,22 +7,6 @@ static uintptr_t pm_start, pm_end;
 //my
 #define MIN_LEN 12
 #define NEXT(x) ((palloc_node)((uintptr_t)x+x->size))
-
-#define LOCKDEF(name) \
-  static volatile intptr_t name##_locked = 0; \
-  static int name##_lock_flags[8]; \
-  void name##_lock() { \
-    name##_lock_flags[_cpu()] = get_efl() & FL_IF; \
-    cli(); \
-    while (1) { \
-      if (0 == _atomic_xchg(&name##_locked, 1)) break; \
-      pause(); \
-    } \
-  } \
-  void name##_unlock() { \
-    _atomic_xchg(&name##_locked, 0); \
-    if (name##_lock_flags[_cpu()]) sti(); \
-  }
 LOCKDEF(alloc)
 
 typedef struct Node{
