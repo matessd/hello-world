4d9e9a3e9fcde564d6b402bce9a70ce5ccaa2904
tracer-nju <tracer@nju.edu.cn>
2019-05-15 15:11:42 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 173d70bccba94963b34eed4c6114c805   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:11:43 up  5:28,  9 users,  load average: 0.04, 0.05, 0.07

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -4,52 +4,70 @@
 static uintptr_t pm_start, pm_end;
 
 //my
-#define MIN_LEN 12
-#define NEXT(x) ((palloc_node)((uintptr_t)x+x->size))
+#define MIN_LEN 16
 LOCKDEF(alloc)
 
 typedef struct Node{
-  struct Node* nxt;
-  size_t size;//存有效数据大小，含结构体 
-  uintptr_t length;//整段长度，含未分配的
+  struct Node* nxt, prev;
+  uintptr_t st,ed;//可分配区间
 }alloc_node,*palloc_node;
-palloc_node a_head, a_tail;
+volatile palloc_node a_head;
 
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
   //my
-  a_head = a_tail = (palloc_node)pm_start;
+  a_head = (palloc_node)pm_start;
   a_head->nxt = NULL;
-  a_head->length = pm_end-pm_start;
-  a_head->size = MIN_LEN;
+  a_head->prev = NULL;
+  a_head->st = ((uintptr_t)a_head)+MIN_LEN;
+  a_head->ed = pm_end;//不可用
 }
 
 static void *kalloc(size_t size) {
   //my
   alloc_lock();
-  assert(size>=0);
+  //assert(size>=0);
+  if(size==0) return NULL;
   palloc_node ret = NULL;
-  uint32_t valid_len = a_tail->length - a_tail->size;
+  palloc_node cur = a_head;
   size_t new_size = size+MIN_LEN;
-  if(valid_len >= new_size){
-    //插入尾部
-    ret = a_tail->nxt = NEXT(a_tail);
-    ret->length = valid_len;
-    ret->size = new_size;
-    a_tail->length=a_tail->size;
-    a_tail = ret;
-  }else{
-    ret = a_head;
-    while(ret!=NULL){
-      
+  while(cur!=NULL){
+    //assert(a_head->ed>=a_head->st);
+    if(cur->ed - cur->st >=new_size){
+      ret = (palloc_node)(cur->ed - new_size);
+      ret->st = ret->ed = cur->ed;
+      cur->ed = (uintptr_t)ret;
+
+      palloc_node nxt = cur->nxt;
+      ret->nxt = nxt;
+      if(nxt!=NULL) nxt->prev = ret;
+
+      ret->prev = cur;
+      cur->nxt = ret;
+      break;
     }
+    cur = cur->nxt;
   }
   alloc_unlock();
-  return ret;
+  return ((uintptr_t)ret)+MIN_LEN;
 }
 
 static void kfree(void *ptr) {
+  //会不会free一个非链表元素？
+  alloc_lock();
+  palloc_node cur = (palloc_node)ptr;
+  if(cur==a_head){
+    cur->st = ((uintptr_t)cur)+MIN_LEN;
+    return;
+  }
+  palloc_node prev = cur->prev;
+  palloc_node nxt = cur->nxt;
+  //assert(prev!=NULL);
+  prev->ed = cur->ed;
+  prev->nxt = nxt;
+  if(nxt!=NULL) nxt->prev = prev;
+  alloc_unlock();
 }
 
 MODULE_DEF(pmm) {
