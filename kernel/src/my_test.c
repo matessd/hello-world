19dd97c856ce89d404d3869c2f5566e553ae43c5
tracer-nju <tracer@nju.edu.cn>
2019-05-15 17:41:46 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 173d70bccba94963b34eed4c6114c805   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  17:41:46 up  7:58, 11 users,  load average: 0.09, 0.20, 0.19

--- kernel/src/my_test.c
+++ kernel/src/my_test.c
@@ -5,10 +5,10 @@ LOCKDEF(test)
 char *ptr[100];
 volatile int i=0;
 void pmm_test(){
-  srand(0);
+  //srand(0);
   //size_t size;
+  test_lock();
   while(i<100){
-    test_lock();
     ptr[i] = pmm->alloc(1000);
     if(ptr[i]==NULL){
       assert(0);
@@ -17,11 +17,11 @@ void pmm_test(){
     sprintf(ptr[i], "hello%d\n",i);
     printf("%d\n",i);
     i++;
-    test_unlock();
   }
   while(i>0){
     printf("%s\n",ptr[--i]);
     pmm->free(ptr[i]);
   }
-  assert(a_head->nxt->nxt->nxt==NULL);
+  assert(a_head->nxt->nxt==NULL);
+  test_unlock();
 }
