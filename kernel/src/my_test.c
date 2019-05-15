8cd49dcb381b499e60f6cfd0e03dee62c9b9e79a
tracer-nju <tracer@nju.edu.cn>
2019-05-15 16:58:17 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 173d70bccba94963b34eed4c6114c805   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  16:58:18 up  7:14, 11 users,  load average: 0.16, 0.13, 0.11

--- kernel/src/my_test.c
+++ kernel/src/my_test.c
@@ -8,16 +8,16 @@ void pmm_test(){
   srand(0);
   //size_t size;
   while(i<1024){
+    test_lock();
     ptr[i] = pmm->alloc(1000);
     if(ptr[i]==NULL){
       assert(0);
       return;
     }
-    test_lock();
     sprintf(ptr[i], "hello%d\n",i);
     printf("%s\n",ptr[i]);
-    test_unlock();
     i++;
+    test_unlock();
   }
   while(i>0){
     printf("%s\n",ptr[--i]);
