13d797b22cfee03375e02d57ae11b3e06519d32d
tracer-nju <tracer@nju.edu.cn>
2019-05-16 14:56:47 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: adfcafdfb2ee48b68762b89c6765e28c   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  14:56:47 up  7:00,  9 users,  load average: 0.32, 0.18, 0.18

--- kernel/include/my_os.h
+++ kernel/include/my_os.h
@@ -34,7 +34,7 @@ volatile palloc_node a_head;
 typedef struct Cli_state{
   int ncli, intena;
 }cli_sta;
-extern cli_sta cpu_cli[8];
+extern volatile cli_sta cpu_cli[8];
 
 extern spinlock_t *os_lk;
 
