07fd6931a9f7f62c4def6a96f96b96310a9f5de3
tracer-nju <tracer@nju.edu.cn>
2019-05-17 22:06:09 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 7aa5dd23e13b4eb792263547e59bf249   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  22:06:09 up 13:01,  9 users,  load average: 0.03, 0.13, 0.16

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -1,8 +1,8 @@
 #include<my_os.h>
 
 void kmt_init(){
-  os->on_irq(INT_MIN, _EVENT_NULL, kmt_context_save); // 总是最先调用
-  os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch); // 总是最后调用
+  os->on_irq(-100, _EVENT_NULL, kmt_context_save); // 总是最先调用
+  os->on_irq(100, _EVENT_NULL, kmt_context_switch); // 总是最后调用
   return;
 }
 int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg);
