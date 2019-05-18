407cea2253d4d46d1a0977334cfa7caec18e7517
tracer-nju <tracer@nju.edu.cn>
2019-05-18 13:08:19 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: df1220a56d7a4fd5ae317612ff16ec40   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  13:08:19 up  4:57,  9 users,  load average: 0.32, 0.26, 0.21

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -15,7 +15,7 @@ int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *a
   task->sleep_flg = 0;
   task->fence = FENCE;
   int i = ntask++ %_ncpu();
-  printf("%d^^\n",i)
+  printf("%d^^\n",i);
   kmt->spin_unlock(create_lk);
 
   add_head(task,i);
