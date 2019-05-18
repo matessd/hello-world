b0a81508da7bc2f8f836a2026147783937075ce0
tracer-nju <tracer@nju.edu.cn>
2019-05-18 22:38:08 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: df1220a56d7a4fd5ae317612ff16ec40   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  22:38:08 up 14:27,  9 users,  load average: 0.18, 0.11, 0.09

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -16,7 +16,7 @@ int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *a
 
   kmt->spin_lock(task_lk);
   int cnt = Ntask++%_ncpu();
-  //printf("%d**\n",ntask);
+  printf("%d**\n",cnt);
   task->id = cnt;
   //如果多处理器好了这样会不会有问题？
   //add_head(task,cnt);
