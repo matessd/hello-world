74f4f99ed2a12407f6dd1a7867c7370afca128e4
tracer-nju <tracer@nju.edu.cn>
2019-05-18 15:48:48 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: df1220a56d7a4fd5ae317612ff16ec40   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:48:48 up  7:38,  9 users,  load average: 0.31, 0.21, 0.25

--- kernel/src/sem.c
+++ kernel/src/sem.c
@@ -8,8 +8,8 @@ void sem_init(sem_t *sem, const char *name, int value){
 }
 void sem_wait(sem_t *sem){
   kmt->spin_lock(&sem->lk);
-  //printf("wait: %s\n",current->name);
-  //printf("value: %d\n\n",sem->value);
+  printf("wait: %s\n",current->name);
+  printf("value: %d\n\n",sem->value);
   sem->value--;
   if (sem->value < 0) {
     assert(task_head!=NULL);
