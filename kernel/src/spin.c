1f523538e4374d5c6ab32f893db9ce175945a61a
tracer-nju <tracer@nju.edu.cn>
2019-05-18 19:07:51 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: df1220a56d7a4fd5ae317612ff16ec40   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  19:07:52 up 10:57,  9 users,  load average: 0.07, 0.14, 0.13

--- kernel/src/spin.c
+++ kernel/src/spin.c
@@ -18,8 +18,11 @@ void spin_init(spinlock_t *lk, const char *name){
 
 void spin_lock(spinlock_t *lk){
   pushcli(); // disable interrupts to avoid deadlock.
-  if(holding(lk))
-    dpanic("acquire");
+  if(holding(lk)){
+    //dpanic("acquire");
+    printf("acquire: %s\n",lk-name);
+    _halt(1);
+  }
 
   // The xchg is atomic.
   while(_atomic_xchg(&lk->locked, 1) != 0);
