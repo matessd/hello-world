2dc2abc6ee1a2f75a258acaf3d07aca056672a3f
tracer-nju <tracer@nju.edu.cn>
2019-05-18 23:22:52 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: df1220a56d7a4fd5ae317612ff16ec40   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  23:22:52 up 15:12,  9 users,  load average: 0.36, 0.30, 0.32

--- kernel/src/spin.c
+++ kernel/src/spin.c
@@ -18,7 +18,7 @@ void spin_init(spinlock_t *lk, const char *name){
 
 void spin_lock(spinlock_t *lk){
   //printf("lock: %s %d\n",lk->name,_cpu());
-  printf("lock: %d",lk->locked);
+  printf("lock: %d; cpu: %d",lk->locked,lk->cpu);
   pushcli(); // disable interrupts to avoid deadlock.
   if(holding(lk)){
     //dpanic("acquire");
