c785c0df525e2925b0b9ba56bbef514e2ef438a7
tracer-nju <tracer@nju.edu.cn>
2019-06-01 00:10:26 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 68b73a847f6d47dabeea42466851700b   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  00:10:26 up 12:34,  7 users,  load average: 1.04, 0.60, 0.43

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
   
   init();
   //printf("%x**%x\n",(int)(intptr_t)start,(int)(intptr_t)tmp_start);
-  for(int i=0; i<1000; i++){
+  for(int i=0; i<10000; i++){
     unsigned char s = *(unsigned char*)(start+i*32);
     if((uint8_t)s==0xe5) printf("%d\n",i);
   }
