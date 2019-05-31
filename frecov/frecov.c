39f9786e58c4c713c7ff87b581b59728b045b68e
tracer-nju <tracer@nju.edu.cn>
2019-05-31 22:57:03 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 68b73a847f6d47dabeea42466851700b   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  22:57:03 up 11:21,  7 users,  load average: 0.18, 0.21, 0.33

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -9,18 +9,18 @@
 #define MB (1024*1024)
 //扇区sector大小为512字节
 #define SECSZ 512
-//FAT每个表项32字节
-#define FATSZ 32
 #define FATNUM 2
 char *start=NULL;
-
+int FATSZ/*FAT扇区数*/;
 
 int main(int argc, char *argv[]) {
   int fd = open(argv[1], O_RDWR);
   assert(fd!=-1);
   start = mmap(NULL, 64*MB, PROT_READ, MAP_PRIVATE, fd, 0);
   assert((intptr_t)start!=-1);
-  printf("%d\n",*(int *)(start+0x2c));
+  FATSZ = *(short*)(start+0x16);
+  if(FATSZ==0) FATSZ = *(int*)(start+0x24);
+  printf("%d\n",FATSZ);
   munmap(start, 64*MB);
   close(fd);
   //printf("%x\n",(int)(intptr_t)start);
