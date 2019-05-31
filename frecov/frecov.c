3bc50c27ea58a833db0cc37b9849c72be9ecc498
tracer-nju <tracer@nju.edu.cn>
2019-05-31 23:12:29 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 68b73a847f6d47dabeea42466851700b   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  23:12:29 up 11:36,  7 users,  load average: 0.21, 0.24, 0.27

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -11,19 +11,26 @@
 #define SECSZ 512
 #define FATNUM 2
 char *start=NULL;
-int FATSZ/*FAT扇区数*/, RES_SEC/*保留扇区数*/, SEC_PER_CLU/*每簇扇区数，簇：cluster*/, ST_CLU/*起始簇号*/;
+int32_t FAT_SEC/*FAT扇区数*/, RES_SEC/*保留扇区数*/, SEC_PER_CLU/*每簇扇区数，簇：cluster*/, ST_CLU/*起始簇号*/, data_off/*数据区偏移*/;
+
+void init(){
+  FAT_SEC = *(int16_t*)(start+0x16);
+  if(FAT_SEC==0) FAT_SEC = *(int32_t*)(start+0x24);
+  RES_SEC = *(int16_t*)(start+0xe);
+  SEC_PER_CLU = *(int8_t*)(start+0xd);
+  ST_CLU = *(int32_t*)(start+0x2c);
+  data_off = (RES_SEC + FAT_SEC*FATNUM + (ST_CLU-2)*SEC_PER_CLU)*SECSZ;
+  printf("%d\n",data_off);
+}
 
 int main(int argc, char *argv[]) {
   int fd = open(argv[1], O_RDWR);
   assert(fd!=-1);
   start = mmap(NULL, 64*MB, PROT_READ, MAP_PRIVATE, fd, 0);
   assert((intptr_t)start!=-1);
-  FATSZ = *(int16_t*)(start+0x16);
-  if(FATSZ==0) FATSZ = *(int32_t*)(start+0x24);
-  RES_SEC = *(int16_t*)(start+0xe);
-  SEC_PER_CLU = *(int8_t*)(start+0xd);
-  ST_CLU = *(int32_t*)(start+0x2c);
-  printf("%d\n",ST_CLU);
+  
+  init();
+  //printf("%d\n",ST_CLU);
   munmap(start, 64*MB);
   close(fd);
   //printf("%x\n",(int)(intptr_t)start);
