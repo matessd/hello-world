ddaab6159a98ea44d887865b6d2db7e6b1459bdd
tracer-nju <tracer@nju.edu.cn>
2019-05-31 21:45:21 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 68b73a847f6d47dabeea42466851700b   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  21:45:21 up 10:09,  5 users,  load average: 0.36, 0.29, 0.23

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -16,6 +16,6 @@ int main(int argc, char *argv[]) {
   start = mmap(NULL, 64*MB, PROT_READ, MAP_PRIVATE, fd, 0);
   assert((intptr_t)start!=-1);
   close(fd);
-  printf("%u\n",(unsigned)(intptr_t)start);
+  printf("%x\n",(int)(intptr_t)start);
   return 0;
 }
