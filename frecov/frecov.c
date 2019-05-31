b5a11486600858b7f29ca7b3ae5c37daf7ef4c13
tracer-nju <tracer@nju.edu.cn>
2019-05-31 23:04:25 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 68b73a847f6d47dabeea42466851700b   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  23:04:25 up 11:28,  7 users,  load average: 0.50, 0.30, 0.31

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -11,7 +11,7 @@
 #define SECSZ 512
 #define FATNUM 2
 char *start=NULL;
-int FATSZ/*FAT扇区数*/, RES_SEC/*保留扇区数*/, SEC_PER_CLU/*每簇扇区数，簇：cluster*/;
+int FATSZ/*FAT扇区数*/, RES_SEC/*保留扇区数*/, SEC_PER_CLU/*每簇扇区数，簇：cluster*/, ST_CLU/*起始簇号*/;
 
 int main(int argc, char *argv[]) {
   int fd = open(argv[1], O_RDWR);
@@ -22,6 +22,7 @@ int main(int argc, char *argv[]) {
   if(FATSZ==0) FATSZ = *(int32_t*)(start+0x24);
   RES_SEC = *(int16_t*)(start+0xe);
   SEC_PER_CLU = *(int8_t*)(start+0xd);
+  ST_CLU = *(int32_t*)(start+0x2c);
   printf("%d\n",SEC_PER_CLU);
   munmap(start, 64*MB);
   close(fd);
