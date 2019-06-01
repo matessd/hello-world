6d6ff0d446ff520e6acaef015a610312838acc0e
tracer-nju <tracer@nju.edu.cn>
2019-06-01 19:41:23 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 357c1c90c1314c8fb2218f89dd72176f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  19:41:23 up  8:04,  5 users,  load average: 0.18, 0.42, 0.38

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -14,9 +14,10 @@
 #define FATNUM 2
 unsigned char *start=NULL, *tmp_start;
 int32_t fd, FILE_SZ/*文件大小*/, FAT_SEC/*FAT扇区数*/, RES_SEC/*保留扇区数*/, SEC_PER_CLU/*每簇扇区数，簇：cluster*/, ST_CLU/*起始簇号*/, data_off/*数据区偏移*/, RES/*数据区大小*/;
-typedef struct{
+struct{
   char name[15];
-}SDE;
+}NODE1;
+typedef struct NODE1 SDE;
 SDE[200];
 
 int file_size2(char* filename){  
