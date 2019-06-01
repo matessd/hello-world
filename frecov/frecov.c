ae587eec26037b2b6b7192dd0ab5b87d2a1bf646
tracer-nju <tracer@nju.edu.cn>
2019-06-01 22:57:40 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 357c1c90c1314c8fb2218f89dd72176f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  22:57:40 up 11:20,  5 users,  load average: 0.19, 0.31, 0.21

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -140,7 +140,7 @@ void merge_lde(){
     //if(idx==0x41) printf("%s\n",tmp);
     if((idx&0x40)==0){
       for(int j=i-1; j>=0; j--){
-        if(((lde[j].idx&0x1f)==(idx&0x1f)+1) 
+        if(((lde[j].idx&0x1f)!=(idx&0x1f)+1) 
            || lde[j].checksum!=checksum)
            continue;
         lde[j].vis=1;
