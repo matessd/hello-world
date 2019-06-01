c0f1c225583cb4e8497ee53b62eb4acde80b4743
tracer-nju <tracer@nju.edu.cn>
2019-06-01 20:44:57 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 357c1c90c1314c8fb2218f89dd72176f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  20:44:57 up  9:08,  5 users,  load average: 0.43, 0.29, 0.29

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -17,7 +17,7 @@
 unsigned char *start=NULL, *tmp_start;
 int32_t fd, FILE_SZ/*文件大小*/, FAT_SEC/*FAT扇区数*/, RES_SEC/*保留扇区数*/, SEC_PER_CLU/*每簇扇区数，簇：cluster*/, ST_CLU/*起始簇号*/, data_off/*数据区偏移*/, RES/*数据区大小*/;
 typedef struct{
-  char name[15];
+  unsigned char name[15];
 }SDE;
 SDE sde[200];
 int scnt;
@@ -56,12 +56,17 @@ void find_sde(){
     if(cur[0xc]==0 &&cur[0xb]==0x20) {
       //if(*cur==0xe5) printf("%c**",*cur);
       sprintf(sde[++scnt].name,"%s",cur);
-      sde[scnt].name[11]='\0';
-      //printf("%s\n",sde[scnt].name);
+      for(int i=0; i<8; i++){
+        if(sde[scnt].name[i]==0x20){
+          sde[scnt].name[i] = '\0';
+          strcat(sde[scnt].name, "BMP");
+          break;
+        }
+      }
+      //sde[scnt].name[11]='\0';
+      printf("%s\n",sde[scnt].name);
     }
     /*if(cur[0x8]==0x42 &&cur[0x9]==0x4d &&cur[0xa]==0x50) {
-      printf("%s\n",cur);
-      cnt++;
     }*/
   }
   //printf("scnt:%d\n",scnt);
@@ -76,8 +81,8 @@ void find_lde(){
       if(*cur==0xe5) printf("%c**",*cur);
       //sprintf(sde[++scnt].name,"%s",cur);
       //sde[scnt].name[11]='\0';
-      printf("%x&&",*cur);
-      printf("%s\n",cur+0x3);
+      //printf("%x&&",*cur);
+      //printf("%s\n",cur+0x3);
     }
   }  
 }
