c2bcfe1e2386b5d046f1d7a8c38de37ed0c3e7a3
tracer-nju <tracer@nju.edu.cn>
2019-06-01 19:19:11 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 357c1c90c1314c8fb2218f89dd72176f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  19:19:11 up  7:42,  5 users,  load average: 0.36, 0.44, 0.34

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -49,7 +49,10 @@ int main(int argc, char *argv[]) {
   for(int i=0; i<RES/32; i++){
     cur = start+i*32;
     //if(cur[0xc]==0 &&cur[0xb]==0x20) printf("%x*%x*%x\n",cur[0x8],cur[0x9],cur[0xa]);
-    if(cur[0xc]==0 &&cur[0xb]==0x20) printf("%s\n",cur);
+    if(cur[0xc]==0 &&cur[0xb]==0x20) {
+      printf("%s\n",cur);
+      cnt++;
+    }
     /*if(cur[0x8]==0x42 &&cur[0x9]==0x4d &&cur[0xa]==0x50) {
       printf("%s\n",cur);
       cnt++;
