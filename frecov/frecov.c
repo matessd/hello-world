48f351685321b2fbf6114761fd64df33b8565649
tracer-nju <tracer@nju.edu.cn>
2019-06-01 16:22:52 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 357c1c90c1314c8fb2218f89dd72176f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  16:22:53 up  4:46,  5 users,  load average: 0.22, 0.23, 0.35

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -40,12 +40,12 @@ int main(int argc, char *argv[]) {
   //printf("%x\n",(int)(intptr_t)(start-j*32));
   char s = *start;
   for(int i=0; i<RES/32; i++){
-    //if(i==1031936) printf("1\n");
+    if(i==1031936) printf("%x\n",(int)(intptr_t)(start+i*32));
     s = *(start+i*32);
-    if(i==1031936) printf("1\n");
+    //if(i==1031936) printf("1\n");
     int8_t tmp = 0xe5;
     if((int8_t)s==tmp) printf("%x\n",i);
-    if(i>1020325) printf("%d\n",i);
+    //if(i>1020325) printf("%d\n",i);
   }
   munmap(tmp_start, 64*MB);
   close(fd);
