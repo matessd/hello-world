9b4a4abe6d16d88e4783d4f236c373c0c54a0c2b
tracer-nju <tracer@nju.edu.cn>
2019-06-08 17:07:44 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: c2dee6c28e7e4937a84b3f45ad504d53   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  17:07:44 up 2 days,  1:24,  6 users,  load average: 0.11, 0.19, 0.18

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -18,7 +18,7 @@ int kvdb_close(kvdb_t *db){
 
 int kvdb_put(kvdb_t *db, const char *key, const char *value){
   //db->fix = 0;//开始修改 
-  if(db->ifoen==0) return 1;
+  if(db->ifopen==0) return 1;
   if(fseek(db->fp,0,SEEK_END)!=0) return 2;
   int ret = fprintf(db->fp,"%s %s\n",key,value);
   //db->fix = 1;
@@ -29,11 +29,11 @@ char *kvdb_get(kvdb_t *db, const char *key){
   if(db->ifopen==0) return NULL;
   if(fseek(db->fp,0,SEEK_SET)!=0) return NULL;
   char tkey[130];
-  char *value = malloc(16*1024*1024)//16MB
+  char *value = malloc(16*1024*1024);//16MB
   if(value==NULL) return NULL;
   tkey[0] = '\0';
   while(strcmp(tkey,key)!=0){
-    if(fscanf("%s %s",tkey,value)==EOF)
+    if(fscanf(db->fp,"%s %s",tkey,value)==EOF)
       return NULL;
   }
   return value;
