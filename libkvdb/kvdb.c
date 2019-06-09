12d3835de8f498b8e4815c3e3ad87f1ba360cecf
tracer-nju <tracer@nju.edu.cn>
2019-06-09 11:24:46 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: d4df1e9091ed4c028eba9dbaff07c46e   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:24:46 up  2:32,  5 users,  load average: 0.17, 0.27, 0.28

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -34,7 +34,7 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
   int used=0, cnt=0, ok=0;
   fseek(db->fp,0,SEEK_SET);
   while(1){
-    fscanf(db->fp,"%[^\n]",&tmp);
+    fscanf(db->fp,"%[^\n]",tmp);
     sscanf(tmp,"%d %s %d",&cnt,tkey,&used);
     //printf("*%d*%c*\n",(int)tmpc,tmpc);
     if(strcmp(tkey,key)==0&&used==1) {
