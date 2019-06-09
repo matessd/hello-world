80b6f59da5dc07a79d954eedbb61e13205224fd6
tracer-nju <tracer@nju.edu.cn>
2019-06-09 16:01:38 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: cff52c8c57884a418d2eeca77b708aab   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  16:01:38 up  2:21,  5 users,  load average: 0.02, 0.07, 0.08

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,24 +1,68 @@
 #include "kvdb.h"
 //#include <assert.h>
+int SEEK1 = 16*1024*1024+512;
+int SEEK2 = 4
+
+int recover(kvdb_t *db){
+  fseek(db->fp,SEEK2,SEEK_SET);
+  int case_num = 0, off1=0, off2=0, len;
+  fscanf(db->fp,"%d",case_num);
+  char key[130];
+  char *value = malloc(16*1024*1024);
+  if(value==NULL) return -1;
+  if(case_num==1){
+    fscanf(db->fp,"%d %s",&off1,value);
+    fseek(db->fp,off1,SEEK_SET);
+    fprintf(db->fp,"%s\n",value);
+  }else if(case_num==2){
+    fscanf(db->fp,"%d %d %d %s %s\n",off1,off2,len,key,value);
+    fseek(db->fp,off1,SEEK_SET);
+    fprintf(db->fp"0");
+    fseek(db->fp,off2,SEEK_SET);
+    fprintf(db->fp,"%d %s 1 %s\n",len,key,value);
+  }else if(case_num==3){
+    fscanf(db->fp,"%d %d %s %s",off1,len,key,value);
+    fseek(db->fp,off1,SEEK_SET);
+    fprintf(db->fp,"%d %s 1 %s\n",len,key,value);
+  }else{
+    return -1;
+  }
+  free(value);
+  fseek(db->fp,0,SEEK_SET);
+  fprintf("0 0\n");
+  //if(fsync(db->fd)==-1) return -1;
+  return 0;
+}
 
 int kvdb_open(kvdb_t *db, const char *filename){
   FILE *fp = NULL;
   fp = fopen(filename, "r+");
   if(fp==NULL){
     //no such file
-    if(errno==2) fp = fopen(filename, "w+");
+    if(errno==2){
+      fp = fopen(filename, "w+");
+      if(fp==NULL) return 1;
+      fprintf(fp,"0 0\n");
+      for(int i=SEEK2; i<SEEK1; i++)
+        fputc(' ',fp);
+    }
   }
   //assert(fp!=NULL);
   if(fp==NULL) return 1;
   int fd = fileno(fp);
   //assert(fd!=-1);
   if(fd==-1) return -1;
+
+  //log恢复完
+  fseek(fp,0,SEEK_SET);
+  int a,b;
+  fscanf(fp,"%d %d",&a,&b);
+  if(a==1&&b==1) 
+    if(recover(db))
+      return -1;
   db->fd = fd;
   db->fp = fp;
   db->ifopen = 1;
-  //log恢复完
-  //fseek(fp,0,SEEK_SET);
-  //if(fprintf(fp,"0 0\n")<0) return -2;
   return 0;
 }
 
@@ -38,7 +82,7 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
   char tkey[130], tmpc; 
   tkey[0] = '\0';
   int used=0, cnt=0, ok=0;
-  fseek(db->fp,0,SEEK_SET);
+  fseek(db->fp,SEEK1,SEEK_SET);
   while(1){
     fscanf(db->fp,"%d %s %d%c",&cnt,tkey,&used,&tmpc);
     //printf("*%d*%s*\n",cnt,tkey);
@@ -50,23 +94,34 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
   }
   int len = strlen(value);
   
-  int ret = 0;
+  int ret = 0, off1 = 0, off2 = 0;
   if(ok==1){
     if(len<=cnt){
-      ret = fprintf(db->fp,"%s\n",value);
-    }
-    else {
+      off1 = ftell(db->fp);
+      //ret = fprintf(db->fp,"%s\n",value);
+      fseek(db->fp, SEEK2, SEEK_SET);
+      fprintf(db->fp,"1\n%d %s\n",off1,value);
+    }else {
       if(fseek(db->fp,-2,SEEK_CUR)!=0)
         return 2;
-      fprintf(db->fp,"0");
-      ok = 0;
+      off1 = ftell(db->fp);
+      fseek(db->fp,0,SEEK_END);
+      off2 = ftell(db->fp);
+      fseek(db->fp, SEEK2, SEEK_SET);
+      fprintf(db->fp,"2\n%d \n%d %d %s %s\n",off1,off2,len,key,value);
+      //fpritnf(db->fp,"%d %s 1 %s\n",len,key,value);
     }
-  }
-  if(ok==0){
+  }else{
     fseek(db->fp,0,SEEK_END);
-    ret = fprintf(db->fp,"%d %s 1 %s\n",len,key,value);
+    off1 = ftell(db->fp);
+    fseek(db->fp,SEEK2,SEEK_SET);
+    //ret = fprintf(db->fp,"%d %s 1 %s\n",len,key,value);
+    fprintf(db->fp,"3\n%d %d %s %s\n",off1,len,key,value);
   }
-  if(fsync(db->fd)==-1) return -1;
+  fseek(db->fp,0,SEEK_SET);
+  fprintf("1 1\n");
+  if(recover(db)) return -1;
+  //if(fsync(db->fd)==-1) return -1;
   return ret<0? 3:0;
 }
 
