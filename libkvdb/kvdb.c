d9ecb6d1b7bd7b5adb9a1e0fe7ff5a4bfa3aecb6
tracer-nju <tracer@nju.edu.cn>
2019-06-10 10:52:50 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: c0c0747625624ec288ebbcd518eb4c50   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  10:52:49 up 8 min,  3 users,  load average: 0.10, 0.24, 0.19

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -103,12 +103,6 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
   pthread_mutex_lock(&db->mutex);
   flock(db->fd, LOCK_EX);
   if(db->ifopen==0) return 1;
-  fseek(db->fp,0,SEEK_SET);
-  int a,b;
-  fscanf(db->fp,"%d %d",&a,&b);
-  if(a==1&&b==1) 
-    if(recover(db))
-      return -1;
   char tkey[130], tmpc; 
   tkey[0] = '\0';
   int used=0, cnt=0, ok=0;
@@ -162,12 +156,6 @@ char *kvdb_get(kvdb_t *db, const char *key){
   pthread_mutex_lock(&db->mutex);
   flock(db->fd, LOCK_EX);
   if(db->ifopen==0) return NULL;
-  fseek(db->fp,0,SEEK_SET);
-  int a,b;
-  fscanf(db->fp,"%d %d",&a,&b);
-  if(a==1&&b==1) 
-    if(recover(db))
-      return NULL;
   if(fseek(db->fp,SEEK1,SEEK_SET)!=0) return NULL;
   char tkey[130], tmpc; tkey[0]='\0';
 
