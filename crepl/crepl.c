f693e796579d5a366bf526433296538b4862abfa
tracer-nju <tracer@nju.edu.cn>
2019-06-11 23:23:36 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 97cee7e3db234772827ae9e6570fc1e2   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  23:23:36 up  2:04,  3 users,  load average: 0.10, 0.30, 0.35

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -6,19 +6,21 @@
 int g_cnt = 0;
 int main(int argc, char *argv[]) {
   char s_in[1000], tmp[100], tmpc, *filename;
-  FILE *fp=NULL;
+  //FILE *fp=NULL;
+  int fd;
   while(scanf("%[^\n]%c",s_in,&tmpc)!=EOF){
     sscanf(s_in,"%s",tmp);
     if(strcmp(tmp,"int")==0){
       //sprintf(tmp,"%d.c",++g_cnt);
-      fp = mkstemp("XXXXXX");
+      fd = mkstemp("XXXXXX");
       //printf("%s\n",filename);
       //assert(filename!=NULL);
       //fp = fopen(filename,"r+");
       //fprintf(stderr, "errno: %s\n", strerror(errno));
-      assert(fp!=NULL);
-      filename = fcb[fp->fd].filename;
+      assert(fd!=-1);
+      filename = fcb[fd].filename;
       printf("%s\n",filename);
+      /*assert(0);
       int i = 0;
       while(s_in[i]!='\0'){
         fputc(s_in[i++],fp);
@@ -26,7 +28,7 @@ int main(int argc, char *argv[]) {
       sprintf(tmp,"gcc -c %s",filename);
       system(tmp);
       fclose(fp);
-      printf("%s\n",filename);
+      printf("%s\n",filename);*/
       continue;
     }
     //int (*func)() = func_lookup(id);
