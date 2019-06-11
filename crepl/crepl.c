d3911c9996eda7078b52d5111796c698fb79e5d0
tracer-nju <tracer@nju.edu.cn>
2019-06-11 22:48:24 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 97cee7e3db234772827ae9e6570fc1e2   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  22:48:24 up  1:29,  3 users,  load average: 0.38, 0.41, 0.38

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,3 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+int g_cnt = 0;
 int main(int argc, char *argv[]) {
+  char s_in[1000], tmp[100], tmpc;
+  FILE *fp=NULL;
+  while(scanf("%[^\n]%c",s_in,&tmpc)!=EOF){
+    sscanf(s_in,"%s",tmp);
+    if(strcmp(tmp,"int")==0){
+      //sprintf(tmp,"%d.c",++g_cnt);
+      fp = tmpfile();
+      int i = 0;
+      while(s_in[i]!='\0'){
+        fputc(fp,s_in[i++]);
+      }
+      fclose(fp);
+      continue;
+    }
+    int (*func)() = func_lookup(id);
+    int value = func();
+    printf(">> %s\n",value);
+  }
   return 0;
 }
