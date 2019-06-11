512157525c05851d8e5091b3aa11e5772bfb14b9
tracer-nju <tracer@nju.edu.cn>
2019-06-11 22:52:25 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 97cee7e3db234772827ae9e6570fc1e2   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  22:52:25 up  1:33,  3 users,  load average: 0.20, 0.31, 0.35

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -3,18 +3,20 @@
 #include<assert.h>
 int g_cnt = 0;
 int main(int argc, char *argv[]) {
-  char s_in[1000], tmp[100], tmpc;
+  char s_in[1000], tmp[100], tmpc, filename;
   FILE *fp=NULL;
   while(scanf("%[^\n]%c",s_in,&tmpc)!=EOF){
     sscanf(s_in,"%s",tmp);
     if(strcmp(tmp,"int")==0){
       //sprintf(tmp,"%d.c",++g_cnt);
-      fp = tmpfile();
+      filename = tmpnam("1");
+      fopen("1"."r+");
       int i = 0;
       while(s_in[i]!='\0'){
         fputc(s_in[i++],fp);
       }
       fclose(fp);
+      system("gcc -c 1");
       continue;
     }
     //int (*func)() = func_lookup(id);
