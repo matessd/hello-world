bbdc0d25877b2314ad7bac380a47cb1227fa60c4
tracer-nju <tracer@nju.edu.cn>
2019-06-12 08:54:15 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: c60fab7b626e4e27ad425f2816c1889e   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  08:54:15 up 32 min,  3 users,  load average: 0.28, 0.17, 0.18

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -2,36 +2,57 @@
 #include<string.h>
 #include<assert.h>
 #include<stdlib.h>
-#include <errno.h>
+#include<errno.h>
+#include<dlfcn.h>
 int g_cnt = 0;
-int main(int argc, char *argv[]) {
-  char s_in[1000], tmp[100], tmpc, filename[10];
+char so_name[100][10];
+void *handler[100];
+
+void gen_file(char *s_in){
+  char suffix[]="tmp-XXXXXX", filename[10], tmp[50];
   FILE *fp=NULL;
+  char *tmpname = mktemp(suffix);
+  //fprintf(stderr, "errno: %s\n", strerror(errno));
+  assert(tmpname!=NULL);
+  sprintf(filename,"%s.c",tmpname);
+  sprintf(so_name[gcnt],"%s.so",tmpname);
+  fp = fopen(filename,"a+");
+  assert(fp!=NULL);
+  int i = 0;
+  while(s_in[i]!='\0'){
+    fputc(s_in[i++],fp);
+  }
+  sprintf(tmp,"gcc -shared -fPIC -nostartfiles -o %s %s",so_name,filename);
+  system(tmp);
+  handler[g_cnt] = dlopen(so_name[g_cnt],RTLD_LAZY|RTLD_GLOBAL);
+  assert(handler[g_cnt]!=NULL);
+  unlink(filename);
+  fclose(fp);
+  g_cnt++;
+}
+
+int main(int argc, char *argv[]) {
+  char s_in[1000], tmp[1000], tmpc;
   while(scanf("%[^\n]%c",s_in,&tmpc)!=EOF){
     sscanf(s_in,"%s",tmp);
     if(strcmp(tmp,"int")==0){
-      //sprintf(tmp,"%d.c",++g_cnt);
-      char tt[] = "XXXXXX";
-      char *t = mktemp(tt);
-      //fprintf(stderr, "errno: %s\n", strerror(errno));
-      assert(t!=NULL);
-      sprintf(filename,"%s.c",t);
-      printf("%s\n",filename);
-      fp = fopen(filename,"a+");
-      assert(fp!=NULL);
-      int i = 0;
-      while(s_in[i]!='\0'){
-        fputc(s_in[i++],fp);
-      }
-      sprintf(tmp,"gcc -c %s",filename);
-      system(tmp);
-      fclose(fp);
-      //printf("%s\n",filename);
+      gen_file(s_in);
       continue;
     }
-    //int (*func)() = func_lookup(id);
-    //int value = func();
-    //printf(">> %s\n",value);
+    sprintf("int _exprXXX(){return ",tmp);
+    strcat(tmp,s_in);
+    strcat(tmp,";}");
+    gen_file(tmp);
+    int (*func)() = dlsym(handler[g_cnt-1],"_exprXXX");
+    assert(func!=NULL);
+    int value = func();
+    printf(">> %d\n",value);
+    dlclose(handler[g_cnt-1]);
+    unlink(so_name[--g_cnt]);
+  }
+  for(int i=0; i<g_cnt; i++){
+    dlclose(handler[i]);
+    unlink(so_name[i]);
   }
   return 0;
 }
