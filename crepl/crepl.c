9062260ffa8939a625631dfcb46ad7e4035c4340
tracer-nju <tracer@nju.edu.cn>
2019-06-12 11:14:53 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: c60fab7b626e4e27ad425f2816c1889e   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:14:53 up  2:52,  3 users,  load average: 0.11, 0.28, 0.32

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -6,18 +6,17 @@
 #include<dlfcn.h>
 #include<unistd.h>
 int g_cnt = 0;
-char so_name[100][30];
-void *handler[100];
+void *handler[1000];
 
 void gen_file(char *s_in){
-  char suffix[]="tmp-XXXXXX", filename[20], tmp[100];
+  char suffix[]="tmp-XXXXXX",so_name[20], filename[20], tmp[100];
   FILE *fp=NULL;
   char *tmpname = mktemp(suffix);
   //fprintf(stderr, "errno: %s\n", strerror(errno));
   assert(tmpname!=NULL);
   sprintf(tmp,"%s",tmpname);
   sprintf(filename,"%s.c",tmp);
-  sprintf(so_name[g_cnt],"%s.so",tmp);
+  sprintf(so_name,"%s.so",tmp);
   //printf("%s\n",tmp);
   fp = fopen(filename,"a+");
   assert(fp!=NULL);
@@ -30,15 +29,15 @@ void gen_file(char *s_in){
   //不对，隔断字符串后sprint和strcat也不行
   //但在中间加printf，然后又改回来，
   //它自己就突然好了，什么情况
-  sprintf(tmp,"gcc -w -shared -fPIC -nostartfiles -m32 -o %s %s",so_name[g_cnt], filename);
+  sprintf(tmp,"gcc -w -shared -fPIC -nostartfiles -m32 -o %s %s",so_name, filename);
   //printf("%s*\n",tmp);
   system(tmp);
-  sprintf(tmp,"./%s",so_name[g_cnt]);
+  sprintf(tmp,"./%s",so_name);
   handler[g_cnt] = dlopen(tmp,RTLD_NOW|RTLD_GLOBAL);
   if(handler[g_cnt]==NULL)
     fprintf (stderr, "Error:%s\n", dlerror());
   unlink(filename);
-  unlink(so_name[g_cnt]);
+  unlink(so_name);
   //assert(handler[g_cnt]!=NULL);
   g_cnt++;
 }
@@ -59,14 +58,13 @@ int main(int argc, char *argv[]) {
     int (*func)() = dlsym(handler[g_cnt-1],"_exprXXX");
     if(func==NULL){
       printf("Compile Error\n");
-      dlclose(handler[--g_cnt]);
+      //dlclose(handler[--g_cnt]);
       continue;
     }
     //assert(func!=NULL);
     int value = func();
     printf(">> %d\n",value);
     dlclose(handler[--g_cnt]);
-    //unlink(so_name[--g_cnt]);
   }
   for(int i=0; i<g_cnt; i++){
     dlclose(handler[i]);
