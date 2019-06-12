e2b00e904ab709c5ef1d581c79b2ddface776ee4
tracer-nju <tracer@nju.edu.cn>
2019-06-12 09:46:51 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: c60fab7b626e4e27ad425f2816c1889e   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  09:46:51 up  1:24,  3 users,  load average: 0.23, 0.20, 0.29

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -15,9 +15,9 @@ void gen_file(char *s_in){
   char *tmpname = mktemp(suffix);
   //fprintf(stderr, "errno: %s\n", strerror(errno));
   assert(tmpname!=NULL);
-  sprintf(tmp,"%s",tmpname);
-  sprintf(filename,"%s.c",tmp);
-  sprintf(so_name[g_cnt],"%s.so",tmp);
+  sprintf(tmp,"%s\0",tmpname);
+  sprintf(filename,"%s.c\0",tmp);
+  sprintf(so_name[g_cnt],"%s.so\0",tmp);
   printf("%s\n",filename);
   fp = fopen(filename,"a+");
   assert(fp!=NULL);
@@ -26,13 +26,13 @@ void gen_file(char *s_in){
     fputc(s_in[i++],fp);
   }
   fclose(fp);
-  sprintf(tmp,"gcc -shared -fPIC -nostartfiles -m32 -o %s %s",so_name[g_cnt],filename);
+  sprintf(tmp,"gcc -shared -fPIC -nostartfiles -m32 -o %s %s\0",so_name[g_cnt],filename);
   printf("%s*\n",tmp);
   system(tmp);
-  sprintf(tmp,"./%s",so_name[g_cnt]);
+  sprintf(tmp,"./%s\0",so_name[g_cnt]);
   handler[g_cnt] = dlopen(tmp,RTLD_LAZY|RTLD_GLOBAL);
   if(handler[g_cnt]==NULL)
-    fprintf (stderr, "%s ", dlerror());
+    fprintf (stderr, "error:%s\n", dlerror());
   //unlink(filename);
   //unlink(so_name[g_cnt]);
   //fclose(fp);  
