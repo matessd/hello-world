6ba923f5ab637e46327fa2844a20805d5af857b1
tracer-nju <tracer@nju.edu.cn>
2019-07-27 23:54:39 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 53dca1fc807844009aa27d612c1b068f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  23:54:39 up 1 day,  1:34,  6 users,  load average: 0.23, 0.16, 0.18

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -63,28 +63,43 @@ typedef struct fsops{
   //inode_t *(*lookup)(fs_t *fs, const char *path, int flags);
   //int (*close)(inode_t *inode);
 } fsops_t;
-typedef struct{
-  int8_t inode_map[1024];
+
+#define MAX_inode 1024
+typedef struct fs{
+  int8_t inode_map[MAX_inode];
   int8_t blk_map[1024];
-  int32_t inode_table[1024][16];
-  char data_blk[1024][1024];
+  inode_t inode_tab[MAX_inode];
+  char blk[1024][512];
   fsops_t *ops;
   device_t *dev;
 }fs_t;
 
+#define MAX_DIR 64
+#define DIR_NAME_LEN 64
+typedef struct inode{
+  int32_t blkno;
+  //0 is dir, 1 is fs(block dev), 2 is file, 3 is dev
+  int8_t sta;
+  char name[DIR_NAME_LEN];
+  fs_t *fs;
+  inode_t *prev;//上一级目录名
+  //int cnt;//subdir NO
+  inode_t *child[MAX_DIR];
+}inode_t;
+
 typedef struct{
   void (*init)();
-  int (*access)(const char *path, int mode);
-  int (*mount)(const char *path, fs_t *fs);
-  int (*unmount)(const char *path);
+  //int (*access)(const char *path, int mode);
+  //int (*mount)(const char *path, fs_t *fs);
+  //int (*unmount)(const char *path);
   int (*mkdir)(const char *path);
   int (*rmdir)(const char *path);
-  int (*link)(const char *oldpath, const char *newpath);
-  int (*unlink)(const char *path);
+  //int (*link)(const char *oldpath, const char *newpath);
+  //int (*unlink)(const char *path);
   int (*open)(const char *path, int flags);
   ssize_t (*read)(int fd, void *buf, size_t nbyte);
   ssize_t (*write)(int fd, void *buf, size_t nbyte);
-  off_t (*lseek)(int fd, off_t offset, int whence);
+  //off_t (*lseek)(int fd, off_t offset, int whence);
   int (*close)(int fd);
 } MODULE(vfs);
 
--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -1,14 +1,86 @@
 #include<my_os.h>
+fs_t *fs_list[16];
 
-void vfs_init(){
-  fs_t *ram = malloc(sizeof(fs_t));
-  ram->blk_map[0] = 0;
-  int len = sizeof(fs_t);
-  printf("%d\n",len);
+void inode_init(inode_t *inode, int32_t no, int8_t sta, const char *name, fst_t fs, inode_t *prev){
+  inode->blkno = no;
+  inode->sta = sta;
+  strcpy(inode->name, name);
+  inode->fs = fs;
+  inode->prev = prev;
 }
 
+void vfs_init(){
+  fs_t *fs = pmm->alloc(sizeof(fs_t));
+  fs_list[0] = fs;
+  fs->inode_map[0] = 1;
+  inode_t *inode = &fs->inode_tab[0];
+  inode_init(inode, (int32_t)-1, 0, "/", fs, NULL);
+  //sprintf(ram0->inode_tab[0].name, "/");
+}
+
+int valid_inode(fs_t *fs){
+  for(int i=0; i<MAX_inode; i++){
+    if(fs->inode_map[i]==0) return i;
+  }
+  return -1;
+}
+
+int vfs_mkdir(const char *path){
+  //assert(path[0]=='/');
+  
+  //init
+  fs_t *ram = fs_list[0];
+  inode_t *inode = &ram->inode_tab[0];
+  inode_t *child = NULL, prev = NULL;
+  char ctmp[128]; int cur=0;
+  ctmp[0] = '/'; ctmp[1] = '\0';
+  int flg = 0;
+
+  //success or fail
+  for(int i=0; path[i]; i++){
+    if(path[i]=='/' || path[i+1]=='\0'){
+      cur = 0; flg = 0;
+      for(int j=0; j<MAX_DIR; j++){
+        child = inode->child[j];
+        if(child){
+          if(strcmp(child->name, ctmp)==0 && child->sta==0){
+            prev = inode;
+            inode = child; 
+            ram = inode->fs;            
+            flg = 1;
+            break;
+          } 
+        }
+      }
+      if(flg==0 && path[i+1]!='\0'){
+        return 2;//no such dir
+      }
+      if(path[i+1]=='\0'){
+        if(strcmp(inode->name, ctmp)==0)return 1;//already exist
+      }
+    }else{
+      ctmp[cur++] = path[i];
+      ctmp[cur] = '\0';
+    }
+  }
+
+  //success, now create new dir
+  int inodeno = valid_inode(ram);
+  assert(inodeno>0);
+  ram->inode_map[inodeno] = 1;
+  child = &ram->inode_tab[inodeno];
+  inode_init(child, -1, 0, ctmp, ram, inode);
+  for(int i=0; i<MAX_DIR; i++){
+    if(inode->child[i]==NULL){
+      inode->child[i] = child;
+      break;
+    }
+  }
+  return 0;
+}
 
 MODULE_DEF(vfs) {
   .init = vfs_init,
+  .mkdir = vfs_mkdir,
 };
 
