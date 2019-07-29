d041e7949309ded0612a0e9fb6976929b7da86f9
tracer-nju <tracer@nju.edu.cn>
2019-07-29 21:13:08 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: ef65c5c71ace40188de236ed5e8db392   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  21:13:08 up 10:15,  9 users,  load average: 0.81, 0.42, 0.37

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -117,7 +117,7 @@ inode_t *find_inode(const char *path){
   return NULL;
 }
 
-int vfs_mkdir(char *path, int8_t sta, int8_t lmt){
+int vfs_mkdir(const char *path, int8_t sta, int8_t lmt){
   //assert(path[0]=='/');
 
   //init
@@ -133,7 +133,7 @@ int vfs_mkdir(char *path, int8_t sta, int8_t lmt){
   for(int i=1; path[i]; i++){
     if(path[i]=='/' || path[i+1]=='\0'){
       if(strcmp(path,"/proc/1")==0)
-        printf("%s&&%s\n",ctmp,&path[i]);
+        printf("%s&&%c\n",ctmp,path[i]);
       if(path[i+1]=='\0' && path[i]!='/'){
         ctmp[cur++] = path[i];
         ctmp[cur] = '\0';
