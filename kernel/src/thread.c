f1510299e11cd06fa3ebcffc574b0d6763d5db13
tracer-nju <tracer@nju.edu.cn>
2019-07-29 23:07:19 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: ef65c5c71ace40188de236ed5e8db392   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  23:07:19 up 12:09, 13 users,  load average: 0.09, 0.11, 0.15

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -18,7 +18,8 @@ int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *a
   //vfs 
   //char src[64]; 
   char path[64]; 
-  src[0]='\0'; path[0]='\0';
+  //src[0]='\0'; 
+  path[0]='\0';
   sprintf(path,"/proc/%d\0",Ntask+1);
   vfs->mkdir(path, 0, 1);
   //sprintf(path, "/proc/%d/status", Ntask+1);
