8e8535356f6008c9a315f1bbdcb9f165b49d55d6
tracer-nju <tracer@nju.edu.cn>
2019-07-29 21:10:37 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: ef65c5c71ace40188de236ed5e8db392   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  21:10:37 up 10:12,  9 users,  load average: 0.03, 0.19, 0.30

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -131,9 +131,9 @@ int vfs_mkdir(const char *path, int8_t sta, int8_t lmt){
   //success or fail
   if(strcmp(path,"/")==0) return 1;//already exists
   for(int i=1; path[i]; i++){
-    if(path[i]=='/' || path[i+1]==0){
+    if(path[i]=='/' || path[i+1]=='\0'){
       if(strcmp(path,"/proc/1")==0)
-        printf("%s&&%c\n",ctmp,path[i]);
+        printf("%s&&%s\n",ctmp,&path[i]);
       if(path[i+1]=='\0' && path[i]!='/'){
         ctmp[cur++] = path[i];
         ctmp[cur] = '\0';
