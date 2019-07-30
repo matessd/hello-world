8d71365cac3580e62502d96516ce9a164c588dca
tracer-nju <tracer@nju.edu.cn>
2019-07-30 13:19:05 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 89c46bdaafda4343b468f660532f90fa            Boot ID: 70114335af75445eae7a50c86d18155c   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  13:19:06 up  1:01,  5 users,  load average: 0.09, 0.08, 0.09

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -6,13 +6,15 @@ static uintptr_t pm_start, pm_end;
 //#define INIT_VALUE 0xcccccccc
 spinlock_t Alloc_lk;
 spinlock_t *alloc_lk=&Alloc_lk;
-int memtot;
+int memtot, memfree;
 
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
+  
   memtot = (int)(uintptr_t)_heap.end - (int)(uintptr_t)_heap.start;
-  printf("%d\n",memtot);
+  memfree = memtot;
+
   //my
   a_head = (palloc_t)pm_start;
   a_head->nxt = NULL;
@@ -55,6 +57,15 @@ static void *kalloc(size_t size) {
   }
   if(ret==NULL) return NULL;
 
+  //vfs
+  char src[128], ctmp[128];
+  sprintf(src,"MemTotal:  %d B\n",memtot);
+  strcpy(ctmp, src);
+  memfree -= new_size;
+  sprintf(src,"MemFree:  %d B\n",memfree);
+  strcat(ctmp, src);
+  vfs->write("/proc/meminfo", ctmp, 0);
+
   //清零，为了tty和input.c
   memset(ret+1,0,size);
 
@@ -69,6 +80,16 @@ static void kfree(void *ptr) {
   palloc_t cur = ((palloc_t)ptr)-1;
   assert(cur->fence == FENCE);//保证free有效性
   cur->fence = 0;
+
+  //vfs
+  char src[128], ctmp[128];
+  sprintf(src,"MemTotal:  %d B\n",memtot);
+  strcpy(ctmp, src);
+  memfree += cur->ed - (int)(intptr_t)cur;
+  sprintf(src,"MemFree:  %d B\n",memfree);
+  strcat(ctmp, src);
+  vfs->write("/proc/meminfo", ctmp, 0);
+
   if(cur==a_head){
     cur->st = ((uintptr_t)cur)+MIN_LEN;
     return;
